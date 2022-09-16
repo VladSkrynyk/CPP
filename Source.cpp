8dcf152fb27e76d4455e565ebe80e3ce0d4e4dda
VladSkrynyk <108512047+VladSkrynyk@users.noreply.github.com>
2022-09-16 19:11:36 +0300

    Vlad Skrynyk Laba 2 2.1-2.6   2.14e   2.17j

--- Source.cpp
+++ Source.cpp
@@ -1,176 +1,182 @@
 ﻿#include <stdio.h>
 #include <math.h>
-// Laba1 1.1-1.9   1.18    1.21 1.20
+#define PI 3.141592654
+// Laba 2 2.1.-2.6   2.4e   2.14    2.17j
 
-int multiple2operation(int x) {
-	int x2 = x * x;
-	return x2 * x2;
+double heron(double a, double b, double c) {
+   return sqrt((a + b + c) * (a + b - c) * (a + c - b) * (b + c - a)) / 4;
 }
 
-int multiple3operation(int x) {
-	int x2 = x * x;
-	int x4 = x2 * x2;
-	return x4 * x2;
+double gethypotenuse(double a, double b) {
+    
+    return sqrt(a * a + b * b);
 }
 
-int multiple4operation(int x) {
-	int x2 = x * x;
-	int x4 = x2 * x2;
-	int x8 = x4 * x4;
-	return x8 * x;
+double task4a(double x) {
+    double y = x * x + 1;
+    return y * y;
 }
 
-int multiple5operation(int x) {
-	int x2 = x * x;
-	int x3 = x2 * x;
-	int x6 = x3 * x3;
-	int x12 = x6 * x6;
-	return x12 * x3;
+double task4b(double x) {
+    double x1 = x + 1;
+    return pow(x, 3) * x1 + x * x1 + 1;
 }
 
-int multiple6operation(int x) {
-	int x2 = x * x;
-	int x4 = x2 * x2;
-	int x8 = x4 * x4;
-	int x16 = x8 * x8;
-	int x24 = x16 * x8;
-	return x24 * x4;
+double task4c(double x) {
+    double x4 = pow(x, 4);
+    double x2 = pow(x, 2);
+    return x4 * (x + 5) + x2 * (10 *x + 10) + 5 * x + 1;
 }
 
-int secondMultiple6operation(int x) {
-	int x2 = x * x;
-	int x4 = x2 * x2;
-	int x8 = x4 * x4;
-	int x16 = x8 * x8;
-	int x32 = x16 * x16;
-	return x32;
+double task4g(double x) {
+    double x3 = pow(x, 3);
+    return x3 * x3 * x3 + x3 + 1;
 }
 
+double task4d(double x) {
+    double x2 = pow(x, 2);
+    double x3 = pow(x, 3);
+    double x4 = pow(x, 4);
+    return 16 * x4 + 8 * x3 + 4 * x2 + 2 * x + 1;
+}
 
-int main() {
-	// Task1
-	 
-	/*printf("1) - %d\n", 2 + 31);
-	printf("2) - %d\n", 45 * 54 - 11);
-	printf("3) - %d\n", (int)15 / 4);
-	printf("4) - %.2f\n", (float)15.0 / 4);
-	printf("5) - %d\n", 67 % 5);
-	printf("6) - %.1f\n", (2 * 45.1 + 3.2) / 2);*/
-	
-	//Task 2
-	 
-	/*float x1 = 0.00001f;
-	double x2 = 1e-4;
-	long double x3 = pow(10, -4); 
-	double y1 = 24.33e5;
-	double s5 = sqrt(5);
-	double l2 = log(100);
-	printf("%.4f %.4lf %.4Lf %.4f %.4f %.4f", x1, x2, x3, y1, s5, l2);*/
+double my_task4e(double x) {
+    double x2 = x * x;
+    double x4 = x2 * x2;
+    return x * (x4 + x2 + 1);
+}
 
-	//Task 3
-	
-	/*int a;
-	printf("Input number: ");
-	scanf_s("%d", &a);
-	printf("- %d - %d - %d\n %d | %d | %d\n- %d - %d - %d\n", a, a, a, a, a, a, a, a, a);*/
+double Rosenbrock2d(double x, double y) {
+    double z = x * x - y;
+    return 100 * z * z + (x - 1) * (x - 1);
+}
 
-	//Task4
+double length(double x1, double y1, double x2, double y2) {
+    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
 
-	/*float m1, m2, r;
-	float gamma = 6.673 * pow(10, -11);
+double invsqrt(double x, double alpha) {
+    return x / sqrt(1 + alpha * pow(x, 2));
+}
 
-	printf("Input m1: ");
-	scanf_s("%f", &m1);
-	printf("Input m2: ");
-	scanf_s("%f", &m2);
-	printf("Input r: ");
-	scanf_s("%f", &r);
-	
-	printf("F => %e", gamma * ((m1 * m2) / pow(r, 2)));*/
+double invsqrt_derivative(double x, double alpha) {
+    return (sqrt(1 + alpha * pow(x, 2)) - (alpha * pow(x, 2) / sqrt(1 + alpha * pow(x, 2)))) / (1 + alpha * pow(x, 2));
+}
 
-	//Task 5
+int main()
+{
+    //Task 1
 
-	/*int x;
+    /*double arg;
+    printf("Input arg:");
+    scanf_s("%lf", &arg);
+    printf("cos of %.2lf => %.2lf", arg, cos(arg));*/
 
-	printf("Input x: ");
-	scanf_s("%d", &x);
-	
-	printf("a) => %d\n", multiple2operation(x));
-	printf("b) => %d\n", multiple3operation(x));
-	printf("c) => %d\n", multiple4operation(x));
-	printf("d) => %d\n", multiple5operation(x));
-	printf("e) => %d\n", multiple6operation(x));
-	printf("f) => %llu\n", secondMultiple6operation(x));*/
+    //Task 2
 
-	//Task 6
+    /*float a, b;
+    printf("input a b = ");
+    scanf_s("%f %f", &a, &b);
+    printf("Hypotenuse => %f", sqrt(pow(a, 2) + pow(b, 2)));*/
 
-	/*int temparature_с;
-	printf("enter a: ");
-	scanf_s("%d", &temparature_с);
+    /*float a, b, c;
+    printf("input a b = ");
+    scanf_s("%f %f", &a, &b);
 
-	float temparature_f = (float)(temparature_с * 9) / 5 + 32;
-	printf("f=%.0f", temparature_f);*/
+    printf("Hypotenuse => %f", gethypotenuse(a, b));*/
 
-	//Task 7
+    //Task 3
 
-	/*double w;
-	printf("Input real x=");
-	scanf_s("%lf", &w);
+    /*float a, b, c;
+    printf("input a b c = ");
+    scanf_s("%f %f %f", &a, &b, &c);
+    double S = heron(a, b, c);
+    printf("S => %f", S);*/
+    
+    //Task 4
+    
+    //a
+    /*float x;
+    printf("input x = ");
+    scanf_s("%f", &x);
+    printf("Result => %f", task4a(x));*/
 
-	int w1 = (int)w;
-	int w2 = floor(w);
-	int w3 = ceil(w);
-	int w4 = round(w);
+    //b
+    /*float x;
+    printf("input x = ");
+    scanf_s("%f", &x);
+    printf("Result => %f", task4b(x));*/
 
-	printf("%d %d %d %d", w1, w2, w3, w4);*/
+    //c
+    /*float x;
+    printf("input x = ");
+    scanf_s("%f", &x);
+    printf("Result => %f", task4c(x));*/
 
-	//Task 8
+    //g
+    /*float x;
+    printf("input x = ");
+    scanf_s("%f", &x);
+    printf("Result => %f", task4g(x));*/
 
-	/*double x, y;
-	scanf_s("%lf", &x);
-	scanf_s("%lf", &y);
-	printf("%lf %lf", x - y, x * y);*/
+    //d
+    /*float x;
+    printf("input x = ");
+    scanf_s("%f", &x);
+    printf("Result => %f", task4d(x));*/
 
-	//Task 9
+    //e
+    /*float x;
+    printf("input x = ");
+    scanf_s("%f", &x);
+    printf("Result => %f", my_task4e(x));*/
 
-	/*double x, y;
-	printf("Input two numbers: ");
-	scanf_s("%lf %lf", &x, &y);
-	printf("Arithmetic mean => %lf\n", (x + y) / 2);
-	printf("Harmonic mean => %lf\n", 2 / (1 / x + 1 / y));*/
+    //Task 5
 
-	//Task 18
+    /*double x;
+    printf("x=");
+    scanf_s("%lf", &x);
 
-	/*char TicToeArray[9] = {'X', 'O', 'X', ' ', 'X', 'O', 'O', ' ', ' '};
-	
-	for (size_t i = 0; i < sizeof(TicToeArray) / sizeof(char); i++) {
-		printf("Element: %c \n", TicToeArray[i]);
-	}
-	printf("%c | %c | %c\n", TicToeArray[0], TicToeArray[1], TicToeArray[2]);
-	printf("%c | %c | %c\n", TicToeArray[3], TicToeArray[4], TicToeArray[5]);
-	printf("%c | %c | %c\n", TicToeArray[6], TicToeArray[7], TicToeArray[8]);*/
+    printf("y=%lf\n", task4a(x));
 
+    printf("%lf", Rosenbrock2d(0, 0));
 
-	//Task 21
+    if (fabs(Rosenbrock2d(1, 0) + 100) > 0.0000001) {
+        printf("error task 1!");
+    }
 
-	/*int a;
-	printf("Input number: ");
-	scanf_s("%d", &a);
-	printf(" - | - | %d | - | - \n - | %d | %d | %d | -\n %d | %d | %d | %d | %d\n", a, a, a, a, a, a, a, a, a); */
+    if (fabs(Rosenbrock2d(2, 4) + 1) < 0.0000001) {
+        printf("error task 2!");
+    }*/
 
-	//Task 20
-	
-	/*double A, B, C;
-	printf("A=: ");
-	scanf_s("%lf", &A);
-	printf("B=: ");
-	scanf_s("%lf", &B);
-	printf("C=: ");
-	scanf_s("%lf", &C);
+    //Task 6
 
-	printf("Arithmetic mean => %lf\n", (A + B + C) / 3);
-	printf("Harmonic mean => %lf\n", 3 / (1 / A + 1 / B + 1 / C));*/
-	
-	return 0;
+    /*double x1, y1, x2, y2, x3, y3;
 
+    printf("Input A(x, y):\n");
+    scanf_s("%lf %lf", &x1, &y1);
+    printf("Input B(x, y):\n");
+    scanf_s("%lf %lf", &x2, &y2);
+    printf("Input C(x, y):\n");
+    scanf_s("%lf %lf", &x3, &y3);
+
+    double a = length(x1, y1, x2, y2);
+    double b = length(x2, y2, x3, y3);
+    double c = length(x3, y3, x1, y1);
+
+    printf("S => %f", heron(a, b, c));*/
+
+    //Task 14
+    /*float D, a = 3, b = 100, c = 2;
+    D = pow(b, 2) - 4 * a * c;
+    printf("Result 1 => %f\nResult 2 => %f", (-1 * b + sqrt(D)) / 2 * a, (-1 * b - sqrt(D)) / 2 * a);*/
+
+    //Task 17
+    /*float x, alpha;
+    printf("input x alpha: ");
+    scanf_s("%f %f", &x, &alpha);
+    printf("Result 1 => %f\n", invsqrt(x, alpha));
+    printf("Result 2 => %f", invsqrt_derivative(x, alpha));*/
+
+    return 0;
 }
